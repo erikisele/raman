beae1b16c0a0a915294b6329a7b7531a589cc571
Erik Isele <isele@sdfmilan233.sdf.slac.stanford.edu>
2026-05-06 14:23:59 -0700

    updates

---
+++ raman_calculation_3d_sampling_mpi_valence_scan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -t 47:59:59
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --ntasks-per-node=120
+#SBATCH --nodes=4
+#SBATCH --partition=milano
+#SBATCH --account=lcls:tmo100827624
+#SBATCH --output=/sdf/data/lcls/ds/tmo/tmo100827624/results/erik/raman_calculation/logs/log_calcualtion.log
+
+mpirun python raman_calculation_3d_sampling_mpi_valence_scan.py
